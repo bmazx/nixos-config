1e5df6d445ebfc6eb419883d290a9f474a6e2046
bmazx <brianma360@gmail.com>
2026-02-18 01:43:08 -0500

    update to nixos stable branch 25.11

--- flake.nix
+++ flake.nix
@@ -1,14 +1,18 @@
 {
   description = "A very basic flake";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager?ref=release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, home-manager, ...  } @ inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ...  } @ inputs:
   {
     nixosConfigurations.minos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = {
+        inherit inputs;
+      };
       modules = [
         ./host/minos/default.nix
         home-manager.nixosModules.home-manager {
